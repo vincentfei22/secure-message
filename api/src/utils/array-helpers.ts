48ceff492f420ffe3d2961647f6647e916c93aab
vincentfei22 <112939179+vincentfei22@users.noreply.github.com>
2023-08-13 02:10:00 +0930

    Delete api directory

--- api/src/utils/array-helpers.ts
+++
@@ -1,7 +0,0 @@
-export function arrayUnion(array: any[], element: any) {
-  return [...new Set([...array, element])];
-}
-
-export function arrayRemove(array: any[], element: any) {
-  return array.filter((e) => e !== element);
-}
